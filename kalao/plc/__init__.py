8dd9662698466b09898f1748ea18c99ce0329b29
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-01-27 17:11:01 +0100

    Exposing sub-modules in plc __init__

--- kalao/plc/__init__.py
+++ kalao/plc/__init__.py
@@ -1,1 +1,7 @@
 __all__ = ["core", "calibunit", "flipmirror", "shutter"]
+from .core import *
+from .shutter import *
+from .calibunit import *
+from .flipmirror import *
+from .laser import *
+from .tungsten import *
