5b95ce0757a11d43205a4a26e796e2dca67d71d9
malik <malik.algelly@etu.unige.ch>
2021-03-18 22:13:53 +0100

    Modif multi-thread

--- sequencer/seq_command.py
+++ sequencer/seq_command.py
@@ -7,49 +7,47 @@ from kalao.plc import calib_unit
 from kalao.plc import flip_mirror
 from kalao.plc import laser
 from kalao.plc import tungsten
-#from kalao.cacao import "..."
-
-
-PLC_DARK 					= 0x00
-PLC_TUNGSTEN_FLAT 			= 0x01
-PLC_SKY_FLAT 				= 0x02
-PLC_TARGET_OBSERVATION 		= 0x03
-PLC_AO_LOOP_CALIBRATION 	= 0x04
+from kalao.cacao import control
 
 
 def dark():
 	tungsten.off()
+	laser.off()
 	shutter.close()
+	#Take Image
 
 def tungsten_FLAT():
 	shutter.close()
 	tungsten.on()
 	flip_mirror.up()
+	#Select Filter
+	#Take Image
+	tungsten.off()
 
 def sky_FLAT():
 	tungsten.off()
+	laser.off()
 	flip_mirror.down()
 	shutter.open()
-
+	#Select Fitler
+	#Take Image
 
 def target_observation():
 	tungsten.off()
+	laser.off()
 	shutter.open()
 	flip_mirror.down()
-	#...
+	#Select Filter
+	#Centre on target
+	control.close_loop()
+	#Monitor AO and cancel exposure if needed
+	#Take Image
+	shutter.close()
+
 
 def AO_loop_calibration():
 	shutter.close()
 	flip_mirror.up()
 	laser.set_intensity()
-	#"...".start_calib()
+	control.start_calib()
 	laser.set_intensity(0)
-
-
-cmd_dict = {
-	PLC_DARK: 					dark,
-	PLC_TUNGSTEN_FLAT:			tungsten_FLAT,
-	PLC_SKY_FLAT: 				sky_FLAT,
-	PLC_TARGET_OBSERVATION:		target_observation,
-	PLC_AO_LOOP_CALIBRATION: 	AO_loop_calibration
-	}
