a79e7311e0e0e6f874d946af3f2a997ded2dc585
malik <malik.algelly@etu.unige.ch>
2021-03-20 14:36:51 +0100

    add command protocol, multithread func and socket conn to seq

--- sequencer/seq_init.py
+++ sequencer/seq_init.py
@@ -9,32 +9,38 @@ from kalao.plc import flip_mirror
 from kalao.plc import laser
 from kalao.plc import tungsten
 
-def init_FLI_cam():
+
+# Multi-thread fonction: take array of object <function>
+# Create a thread for each function and start the function
+# Block until the end of each thread
+def multi_threading(foncs):
+	threads = []
+
+	for fonc in foncs:
+		th = threading.Thread(target = fonc)
+		th.start()
+		threads.append(th)
+
+	for th in threads:
+		th.join()
+
+
+# Define each init process of some composants
+# Lunch them in multi-thread
+def initialisation():
+
+	def init_FLI_cam():
 	pass
 
-def check_PLC_init_status():
-	calib_return 		= calib_unit.initialise()
-	flip_mirror_return 	= flip_mirror.initialise()
-	shutter_return 		= shutter.initialise()
-	# print return values for control ?
+	def check_PLC_init_status():
+		foncs = [calib_unit.initialise, flip_mirror.initialise, shutter.initialise]
+		multi_threading(foncs)
 
-def init_CACAO():
-	pass
+	def init_CACAO():
+		pass
 
-def init_Shutter():
-	shutter.initialise()
+	def init_Shutter():
+		shutter.initialise()
 
-th_FLI = threading.Thread(target = init_FLI_cam)
-th_PLC = threading.Thread(target = check_PLC_init_status)
-th_CACAO = threading.Thread(target = init_CACAO)
-th_Shutter = threading.Thread(target = init_Shutter)
-
-th_FLI.start()
-th_PLC.start()
-th_CACAO.start()
-th_Shutter.start()
-
-th_FLI.join()
-th_PLC.join()
-th_CACAO.join()
-th_Shutter.join()
+	init_foncs = [init_FLI_cam, check_PLC_init_status, init_CACAO, init_Shutter]
+	multi_threading(init_foncs)
