9d58c5240e9319432d578ed6b2ce8ea79fc30ebe
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-04-15 15:43:51 +0200

    Debugged seq_init

--- sequencer/seq_init.py
+++ sequencer/seq_init.py
@@ -25,7 +25,7 @@ class ThreadWithReturnValue(Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
         Thread.__init__(self, group, target, name, args, kwargs)
         self._return = None
-    def run(self):watch?v=AQ7-qKQMce4
+    def run(self):
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
     def join(self, *args):
