5c8677864ffd39e005ffefb9deb2e89d411196bb
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-04-19 09:55:44 +0200

    star_centering draft

--- kalao/interface/star_centring.py
+++ kalao/interface/star_centring.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Filename : star_centring.py
+# @Filename : star_centering.py
 # @Date : 2021-04-13-17-10
 # @Project: KalAO-ICS
 # @AUTHOR : Janis Hagelberg
 
 """
-star_centring.py is part of the KalAO Instrument Control Software
+star_centering.py is part of the KalAO Instrument Control Software
 (KalAO-ICS). 
 """
 
 from astropy.io import fits
 
+from kalao.utils import database
+
+
 def fli_view():
 
-    fli_image_name = get_latest_image_path()
-    centring_image = fits.getdata(fli_image_name)
+    fli_image_path, file_date = get_temporary_image_path()
+    centering_image = fits.getdata(fli_image_path)[256:768, 256:768]
 
-    manual_centring_needed = False
+    manual_centering_needed = False
 
-    return manual_centring_needed, centring_image
+    return manual_centering_needed, centering_image
+
+
+def _get_image_path(image_type):
+
+    if image_type in ['latest', 'temporary']:
+        # READ mongodb to find latest filename
+        last_image = database.get_obs_log(['fli_'+image_type+'_image_path'], 1)['fli_'+image_type+'_image_path']
+        filename = last_image['values']
+        file_date = last_image['time_utc']
+        if not last_image['values']:
+            # Set to None is list is empty
+            filename = None
+            file_date = None
+
+        return filename, file_date
+
+    return 1
+
 
 def get_latest_image_path():
 
-    # READ mongodb to find latest filename
-    filename = "kALAO.fits"
+    filename, file_date = _get_image_path('latest')
 
-    return filename
+    return filename, file_date
 
-def star_position_pixels(x, y):
+
+def get_temporary_image_path():
+
+    filename, file_date = _get_image_path('temporary')
+
+    return filename, file_date
+
+
+def star_pixel(x, y):
 
     # save x,y into mongodb
     # set manual_centring_needed to false
