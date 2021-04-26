6b0cfbf8ad364fb7bbe5756c6fbf2deb65ad038b
alesina <fabien.alesina@unige.ch>
2021-04-26 16:58:48 +0200

    flask gui added in gitlab project

---
+++ gui_flask/app/routes.py
@@ -0,0 +1,44 @@
+from app import app
+
+from flask_cors import CORS
+from flask import request
+
+from numpy.random import seed
+from numpy.random import randint
+
+import json
+import sys
+sys.path.append('includes/kalao-ics')
+
+from kalao.interface import status as k_status
+
+CORS(app)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return k_status.short()
+@app.route('/status', methods=['GET'])
+def status():
+    return k_status.short()
+@app.route('/PLCData', methods=['GET'])
+def PLCData():
+    return '{"shutter":"OPEN"}'
+
+@app.route('/pixelImages', methods=['GET'])
+def pixelImages():
+    random = bool(request.args.get('random', default = "", type = str))
+    return k_status.cacao_streams(random)
+
+@app.route('/measurements', methods=['GET'])
+def measurements():
+    random = bool(request.args.get('random', default = "", type = str))
+    return k_status.cacao_measurements(random)
+
+@app.route('/plot', methods=['GET'])
+def plot():
+    random = bool(request.args.get('random', default = "", type = str))
+    return k_status.cacao_measurements(random)
+app.run(host="0.0.0.0", port="80");
+#app.run(host= '10.194.67.128')
