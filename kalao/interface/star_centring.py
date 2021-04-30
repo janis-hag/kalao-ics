c2ddbd24b5cb78455c15c2ead551f8df296d6e40
Fabien.Alesina <fabien.alesina@unige.ch>
2021-04-30 14:06:01 +0000

    fakeData for FLI view added

--- kalao/interface/star_centring.py
+++ kalao/interface/star_centring.py
@@ -17,19 +17,22 @@ import numpy as np
 from kalao.utils import database
 
 
-def fli_view():
+def fli_view(realData=False):
 
-    #TODO return empty array if no image exists
-    fli_image_path, file_date = get_temporary_image_path()
+	if not realData:
+		# Returning fake fli_view for testing purposes
+		return fake_data.fake_fli_view()
+	else:
+        fli_image_path, file_date = get_temporary_image_path()
 
-    if fli_image_path is not None and file_date is not None and os.path.isfile(fli_image_path):
-        centering_image = fits.getdata(fli_image_path)
-    else:
-        centering_image = np.zeros((1024, 1204))
+        if fli_image_path is not None and file_date is not None and os.path.isfile(fli_image_path):
+            centering_image = fits.getdata(fli_image_path)
+        else:
+            centering_image = np.zeros((1024, 1204))
 
-    manual_centering_needed = False
+        manual_centering_needed = False
 
-    return manual_centering_needed, centering_image
+        return manual_centering_needed, centering_image
 
 
 def _get_image_path(image_type):
