a89e25f815a25457e0f512b4337b7f1a27b4bbdb
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-05-05 16:35:35 +0200

    Added status reply in gop server

--- tcs_communication/GOP_server.py
+++ tcs_communication/GOP_server.py
@@ -1,15 +1,21 @@
 # test_server.py
 #
-import sys, os
-import logging
+import sys
 import time
 import socket
+from os import path
+from configparser import ConfigParser
 
-sys.path.append("/home/weber/src/pymod_libgop/")
+from pathlib import Path
+import os
+
+config_path = os.path.join(Path(os.path.abspath(__file__)).parents[1], 'kalao.config')
+
+sys.path.append(path.dirname(path.abspath(path.dirname(__file__))))
+
+from kalao.interface import status
 
 import pymod_libgop
-
-sys.path.append("/home/weber/src/tcs_srv/")
 
 import tcs_srv_gop
 
@@ -18,98 +24,101 @@ import tcs_srv_gop
 #
 gop = tcs_srv_gop.gop()
 socketName = "test_server"
-socketPort = 18234    # only for inet connection
+socketPort = 18234  # only for inet connection
 #
 verbosity = 3
 gop.processesRegistration(socketName)
 
-print("%.6f"%(time.time()), "Initialize new gop connection. Wait for client ...")
-#gc = gop.initializeGopConnection(socketName, verbosity)
+print("%.6f" % (time.time()), "Initialize new gop connection. Wait for client ...")
+# gc = gop.initializeGopConnection(socketName, verbosity)
 gc = gop.initializeInetGopConnection(socketName, socketPort, verbosity)
 #
 # Infinite loop, waiting for command
 # Rem; all command reply an acknowledgement
 #
 while (True):
-  print("")
-  print("%.6f"%(time.time()), "Wait for command")
-  #
-  # read and concat until "#" char, then parse the input string
-  #
-  command     = ""
-  controlRead = ""
-  while '#' not in controlRead:
-    controlRead = gop.read()
+    print("")
+    print("%.6f" % (time.time()), "Wait for command")
+    #
+    # read and concat until "#" char, then parse the input string
+    #
+    command = ""
+    controlRead = ""
+    while '#' not in controlRead:
+        controlRead = gop.read()
 
-    if(controlRead == -1):
-      print("%.6f"%(time.time()), "Initialize new gop connection. Wait for client ...")
-      #gc = gop.initializeGopConnection(socketName, verbosity)
-      gc = gop.initializeInetGopConnection(socketName, socketPort, verbosity)
-      break
+        if (controlRead == -1):
+            print("%.6f" % (time.time()), "Initialize new gop connection. Wait for client ...")
+            # gc = gop.initializeGopConnection(socketName, verbosity)
+            gc = gop.initializeInetGopConnection(socketName, socketPort, verbosity)
+            break
 
-    command += controlRead # concat input string
+        command += controlRead  # concat input string
 
-  if(controlRead == -1):
-    continue # go to beginning
+    if (controlRead == -1):
+        continue  # go to beginning
 
-  print ("%.6f"%(time.time()), "AFter gop.read() := ", command)
+    print("%.6f" % (time.time()), "After gop.read() := ", command)
 
-  seperator   = command[0]
-  command     = command[1:]
-  commandList = command.split(seperator)
-  #
-  # 'command' is commandList[0], 'arguments' are commandList[1:]
-  #
-  print ("%.6f"%(time.time()), " command=>", commandList[0], "< arg=",commandList[1:], sep="")
+    seperator = command[0]
+    command = command[1:]
+    commandList = command.split(seperator)
+    #
+    # 'command' is commandList[0], 'arguments' are commandList[1:]
+    #
+    print("%.6f" % (time.time()), " command=>", commandList[0], "< arg=", commandList[1:], sep="")
 
-  # Check if its a KalAO command and send it
-  if(commandList[0][:3] == "kal"):
-    parser = ConfigParser()
-    parser.read('../kalao.config')
+    # Check if its a KalAO command and send it
+    if (commandList[0][:3] == "kal"):
+        parser = ConfigParser()
+        parser.read(config_path)
 
-    host = parser.get('SEQ','IP')
-    port = parser.getint('SEQ','Port')
+        host = parser.get('SEQ', 'IP')
+        port = parser.getint('SEQ', 'Port')
 
-    hostSeq, portSeq = (host, port)
-    socketSeq = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        hostSeq, portSeq = (host, port)
+        socketSeq = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try:
-      socketSeq.connect((hostSeq,portSeq))
-      print("%.6f"%(time.time()), "Connected to sequencer")
+        try:
+            socketSeq.connect((hostSeq, portSeq))
+            print("%.6f" % (time.time()), "Connected to sequencer")
 
-      commandKal = seperator + command
-      socketSeq.sendall(commandKal.encode("utf8"))
-      print("%.6f"%(time.time()), "Command sent")
+            commandKal = seperator + command
+            socketSeq.sendall(commandKal.encode("utf8"))
+            print("%.6f" % (time.time()), "Command sent")
 
-    except ConnectionRefusedError:
-      print("%.6f"%(time.time()), "Error: connection to sequencer refused")
-    finally:
-      socketSeq.close()
+        except ConnectionRefusedError:
+            print("%.6f" % (time.time()), "Error: connection to sequencer refused")
+        finally:
+            socketSeq.close()
 
-  #
-  # Manage the command. 2 cases:
-  # - its a state machine (with a nodeId wich starts with "ns=")
-  # - its a local command (test, exit, ...)
-  #
+    #
+    # Manage the command. 2 cases:
+    # - its a state machine (with a nodeId wich starts with "ns=")
+    # - its a local command (test, exit, ...)
+    #
 
-  if (commandList[0] == "test"):
-    message = "/OK"
-    print("%.6f"%(time.time()), "Send acknowledge: ", message)
-    gop.write(message)
-  elif ((commandList[0] == "quit") or (commandList[0] == "exit")):
-    message = "/OK"
-    print("%.6f"%(time.time()), "Send acknowledge and quit: ", message)
-    gop.write(message)
-    print("%.6f"%(time.time()), "Acknowledge sended")
-    break
-  else:
-    message = "/OK"
-    print("%.6f"%(time.time()), "Send acknowledge: ", message)
-    gop.write(message)
-
+    if commandList[0] == "test":
+        message = "/OK"
+        print("%.6f" % (time.time()), "Send acknowledge: ", message)
+        gop.write(message)
+    elif ((commandList[0] == "quit") or (commandList[0] == "exit")):
+        message = "/OK"
+        print("%.6f" % (time.time()), "Send acknowledge and quit: ", message)
+        gop.write(message)
+        print("%.6f" % (time.time()), "Acknowledge sended")
+        break
+    elif commandList[0] == "status":
+        message = status.kalao_status()
+        print("%.6f" % (time.time()), "Send status: ", message)
+        gop.write(message)
+    else:
+        message = "/OK"
+        print("%.6f" % (time.time()), "Send acknowledge: ", message)
+        gop.write(message)
 #
 # in case of break, we disconnect all serveurs
 #
-print ("%.6f"%(time.time()), socketName, " close gop connection and exit")
+print("%.6f" % (time.time()), socketName, " close gop connection and exit")
 gop.closeConnection()
 sys.exit(0)
