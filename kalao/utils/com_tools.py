0ca57aa79b353b37e633c7c8f543f5783a2e3d4e
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-05-07 15:43:30 +0200

    Created com_tools package

---
+++ kalao/utils/com_tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#@Filename : com_tools.py
+#@Date : 2021-05-07-15-35
+#@Project: KalAO-ICS
+#@AUTHOR : Janis Hagelberg
+
+"""
+com_tools.py is part of the KalAO Instrument Control Software
+(KalAO-ICS). 
+"""
