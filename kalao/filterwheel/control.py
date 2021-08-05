2c286f848190c60ae39b1931aa6564820277562a
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-08-05 10:17:02 +0200

    Updated filter names

--- kalao/filterwheel/control.py
+++ kalao/filterwheel/control.py
@@ -14,18 +14,53 @@ from microscope.filterwheels import thorlabs
 import time
 
 # clear griz, hole
+# 0 clear
+# 1 jaune g
+# 2 violet r
+# 3 bleu clair i
+# 4 argent z
+# 5 empty
 
-def g():
-    # move to filter g
 
-def r():
-    # move to filter r
 
-fw = thorlabs.ThorlabsFW102C(com='/dev/ttyUSB0')
-fw.enable()
-time.sleep(2)
-fw.initialize()
-time.sleep(2)
-fw.set_position(0)
-time.sleep(6)
-fw.get_position()
+def set_position(filter):
+
+    if filter in range(0,6):
+        filter_position = filter
+    elif filter in:
+
+    fw = thorlabs.ThorlabsFW102C(com='/dev/ttyUSB0')
+    fw.enable()
+    time.sleep(2)
+    fw.initialize()
+    time.sleep(2)
+    fw.set_position(filter_position)
+    time.sleep(6)
+    position = fw.get_position()
+
+    if position == filter_position:
+        return 0
+    else:
+        return 1
+
+def get_position():
+    fw = thorlabs.ThorlabsFW102C(com='/dev/ttyUSB0')
+    fw.enable()
+    time.sleep(2)
+    fw.initialize()
+    time.sleep(2)
+    fw.set_position(0)
+    time.sleep(6)
+    fw.get_position()
+
+    return position
+
+
+def init():
+    fw = thorlabs.ThorlabsFW102C(com='/dev/ttyUSB0')
+    fw.enable()
+    time.sleep(2)
+    fw.initialize()
+    time.sleep(2)
+
+    return 0
