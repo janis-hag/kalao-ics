66ceb240b67d8089e9a37cdc1ec1381563dba454
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-08-12 14:48:16 +0200

    Added ADC debugged.

--- kalao/plc/adc.py
+++ kalao/plc/adc.py
@@ -12,7 +12,7 @@ adc.py is part of the KalAO Instrument Control Software
 
 from . import core
 import numbers
-from opcua import Client, ua
+from opcua import ua
 from time import sleep
 from configparser import ConfigParser
 from pathlib import Path
@@ -23,34 +23,31 @@ config_path = os.path.join(Path(os.path.abspath(__file__)).parents[2], 'kalao.co
 parser = ConfigParser()
 parser.read(config_path)
 
-def _get_path(adc_id):
-    if adc_id = 1:
-        adc_path = 'MAIN.ADC1_Newport_PR50PP'
-    elif adc_id = 2:
-        adc_path = 'MAIN.ADC2_Newport_PR50PP'
-    else:
-        return -1
+
+adc_name = {
+    1: 'ADC1_Newport_PR50PP',
+    2: 'ADC2_Newport_PR50PP'}
+
 
 def rotate(adc_id, position=0):
-
-    adc_path = _get_path(adc_id)
 
     # Connect to OPCUA server
     beck = core.connect()
     # define commands
-    motor_nCommand = beck.get_node("ns=4; s=' + adc_path + '.ctrl.nCommand")
-    motor_bExecute = beck.get_node("ns=4; s=' + adc_path + '.ctrl.bExecute")
+    motor_nCommand = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.nCommand")
+    motor_bExecute = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.bExecute")
 
     # Check if initialised
-    init_result = initialise(force_init=False, beck=beck, motor_nCommand=motor_nCommand)
+    init_result = initialise(adc_id, force_init=False, beck=beck, motor_nCommand=motor_nCommand,
+                             motor_bExecute=motor_bExecute)
     if not init_result == 0:
         return init_result
 
     # Set velocity to 1 in case is has been changed
-    motor_lrVelocity = beck.get_node("ns=4; s=' + adc_path + '.ctrl.lrVelocity")
+    motor_lrVelocity = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.lrVelocity")
     motor_lrVelocity.set_attribute(ua.AttributeIds.Value,
                                    ua.DataValue(ua.Variant(float(1), motor_lrVelocity.get_data_type_as_variant_type())))
-    motor_lrPosition = beck.get_node("ns=4; s=' + adc_path + '.ctrl.lrPosition")
+    motor_lrPosition = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.lrPosition")
 
     if isinstance(position, numbers.Number):
         # Set target position
@@ -63,12 +60,12 @@ def rotate(adc_id, position=0):
                                                            motor_nCommand.get_data_type_as_variant_type())))
         # Execute
         send_execute(motor_bExecute)
-        while(beck.get_node("ns=4; s=' + adc_path + '.stat.sStatus").get_value() == 'MOVING in Positioning Mode'):
+        while beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.sStatus").get_value() == 'MOVING in Positioning Mode':
             print('.')
             sleep(5)
         # Get new position
-        new_position = beck.get_node("ns=4; s=' + adc_path + '.stat.lrPosActual").get_value()
-        # motor_lrPosition = beck.get_node("ns=4; s=' + adc_path + '.ctrl.lrPosition")
+        new_position = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.lrPosActual").get_value()
+        # motor_lrPosition = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.lrPosition")
     else:
         print('Expected position to be a number, received: ' + str(position))
         new_position = -1
@@ -92,16 +89,14 @@ def status(adc_id, beck=None):
     # else:
     #     disconnect_on_exit = False
 
-    adc_name = _get_path(adc_id).split('.')[-1]
-
-    status_dict = core.device_status(adc_name, beck=beck)
-    # status_dict = {'sStatus': beck.get_node("ns=4; s=' + adc_path + '.stat.sStatus").get_value(),
-    #                'sErrorText': beck.get_node("ns=4; s=' + adc_path + '.stat.sErrorText").get_value(),
-    #                'nErrorCode': beck.get_node("ns=4; s=' + adc_path + '.stat.nErrorCode").get_value(),
-    #                'lrVelActual': beck.get_node("ns=4; s=' + adc_path + '.stat.lrVelActual").get_value(),
-    #                'lrVelTarget': beck.get_node("ns=4; s=' + adc_path + '.stat.lrVelTarget").get_value(),
-    #                'lrPosActual': beck.get_node("ns=4; s=' + adc_path + '.stat.lrPosActual").get_value(),
-    #                'lrPosition': beck.get_node("ns=4; s=' + adc_path + '.ctrl.lrPosition").get_value()}
+    status_dict = core.device_status(adc_name[adc_id], beck=beck)
+    # status_dict = {'sStatus': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.sStatus").get_value(),
+    #                'sErrorText': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.sErrorText").get_value(),
+    #                'nErrorCode': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.nErrorCode").get_value(),
+    #                'lrVelActual': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.lrVelActual").get_value(),
+    #                'lrVelTarget': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.lrVelTarget").get_value(),
+    #                'lrPosActual': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.lrPosActual").get_value(),
+    #                'lrPosition': beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.lrPosition").get_value()}
 
     # if disconnect_on_exit:
     #     beck.disconnect()
@@ -111,57 +106,55 @@ def status(adc_id, beck=None):
 
 def check_error(adc_id, beck):
 
-    adc_path = _get_path(adc_id)
 
-    if beck.get_node("ns=4; s=' + adc_path + '.stat.sErrorText").get_value() == 0:
+    if beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.sErrorText").get_value() == 0:
         return 0
     else:
-        error_status = 'ERROR'
-        return
+        return 'ERROR'
 
 
-def initialise(adc_id, force_init=False, beck=None, motor_nCommand=None):
-    '''
+def initialise(adc_id, force_init=False, beck=None, motor_nCommand=None, motor_bExecute=None):
+    """
     Initialise the calibration unit.
 
+    :param motor_bExecute:
+    :param adc_id:
+    :param force_init:
     :param beck: the handle for the plc connection
     :param motor_nCommand: handle to send commands to the motor
     :return: returns 0 on success and error code on failure
-    '''
-
-    adc_path = _get_path(adc_id)
+    """
 
     if beck is None:
         # Connect to OPCUA server
         beck = core.connect()
+
     if motor_nCommand is None:
         # define commands
-        motor_nCommand = beck.get_node("ns=4; s=' + adc_path + '.ctrl.nCommand")
+        motor_nCommand = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.nCommand")
 
-    # Set reset on error to true in case it has been changed
-    #motor_bResetError = beck.get_node("ns=4; s=' + adc_path + '.ctrl.bResetError")
-    #motor_bResetError.set_attribute(ua.AttributeIds.Value,
-    #                               ua.DataValue(ua.Variant(True, motor_bResetError.get_data_type_as_variant_type())))
+    if motor_bExecute is None:
+        motor_bExecute = beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".ctrl.bExecute")
 
     # Check if enabled, if no do enable
-    if not beck.get_node("ns=4; s=' + adc_path + '.stat.bEnabled").get_value() or force_init:
-        motor_bEnable = beck.get_node("ns = 4; s = ' + adc_path + '.ctrl.bEnable")
+    if not beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.bEnabled").get_value() or force_init:
+        motor_bEnable = beck.get_node("ns = 4; s = MAIN." + adc_name[adc_id] + ".ctrl.bEnable")
         motor_bEnable.set_attribute(
             ua.AttributeIds.Value, ua.DataValue(ua.Variant(True, motor_bEnable.get_data_type_as_variant_type())))
-        if not beck.get_node("ns=4; s=' + adc_path + '.stat.bEnabled").get_value():
-            error = 'ERROR: '+str(beck.get_node("ns=4; s=' + adc_path + '.stat.nErrorCode").get_value())
+        if not beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.bEnabled").get_value():
+            error = 'ERROR: '+str(beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.nErrorCode").get_value())
             return error
 
     # Check if init, if not do init
-    if not beck.get_node("ns=4; s=' + adc_path + '.stat.bInitialised").get_value() or force_init:
+    if not beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.bInitialised").get_value() or force_init:
         send_init(motor_nCommand, motor_bExecute)
         print('Starting calib_unit init.')
         sleep(15)
-        while(beck.get_node("ns=4; s=' + adc_path + '.stat.sStatus").get_value() == 'INITIALISING'):
+        while beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.sStatus").get_value() == 'INITIALISING':
             print('.')
             sleep(15)
-        if not beck.get_node("ns=4; s=' + adc_path + '.stat.bInitialised").get_value():
-            error = 'ERROR: '+str(beck.get_node("ns=4; s=' + adc_path + '.stat.nErrorCode").get_value())
+        if not beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.bInitialised").get_value():
+            error = 'ERROR: '+str(beck.get_node("ns=4; s=MAIN." + adc_name[adc_id] + ".stat.nErrorCode").get_value())
             return error
     return 0
 
@@ -172,7 +165,7 @@ def send_execute(motor_bExecute):
         ua.AttributeIds.Value, ua.DataValue(ua.Variant(True, motor_bExecute.get_data_type_as_variant_type())))
 
 
-def send_init(motor_nCommand):
+def send_init(motor_nCommand, motor_bExecute):
     motor_nCommand.set_attribute(ua.AttributeIds.Value,
                                  ua.DataValue(ua.Variant(int(1), motor_nCommand.get_data_type_as_variant_type())))
     # Execute
