7102b5a6844873caaff663458491c8799fcf6864
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-08-13 15:37:37 +0200

    debugging update_header function

--- kalao/utils/file_handling.py
+++ kalao/utils/file_handling.py
@@ -84,7 +84,7 @@ def update_header(image_path):
         monitoring_status = database.get_monitoring(keys, 1, dt=dt)
         header.set('LASER', monitoring_status['laser']['values'][0], 'short description fro database_definition')
         header.set('SHUTTER', monitoring_status['shutter']['values'][0], 'short description fro database_definition')
-        header.set('TUNGSTEN', monitoring_status['tungsten']['values'],[0] 'short description fro database_definition')
+        header.set('TUNGSTEN', monitoring_status['tungsten']['values'][0], 'short description fro database_definition')
         header.set('ADC1', monitoring_status['adc1']['values'][0], 'short description fro database_definition')
         header.set('ADC2', monitoring_status['adc2']['values'][0], 'short description fro database_definition')
 
