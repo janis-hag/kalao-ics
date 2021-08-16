e1fc6e1f9ce343489439b4422e3ccf99fcc47030
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-08-16 13:40:42 +0200

    Created system module in sequencer

---
+++ sequencer/system.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Filename : system.py
+# @Date : 2021-08-16-13-33
+# @Project: KalAO-ICS
+# @AUTHOR : Janis Hagelberg
+
+"""
+system.py is part of the KalAO Instrument Control Software
+(KalAO-ICS). 
+"""
+
+def check_status():
+    pass
+
+def camera_service():
+    # TODO status, stop, start
+    pass
+
+def database_service():
+    # TODO status, stop, start
+    pass
