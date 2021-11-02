1823297bb8ddc25127e02230385350a21bea51a3
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-11-02 15:41:46 +0100

    Reindexing pandas mongo df

--- kalao/utils/database.py
+++ kalao/utils/database.py
@@ -163,7 +163,7 @@ def read_mongo_to_pandas(dt, days=1, collection='monitoring', no_id=True):
         # Expand the cursor and construct the DataFrame
         appended_df.append(pd.DataFrame(list(cursor)))
 
-    df = pd.concat(appended_df)
+    df = pd.concat(appended_df, ignore_index=True)
 
     # Delete the _id
     if no_id:
