1bca7ab56534b6f723640df333ea03004ea7f156
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-12-20 15:38:55 +0100

    Changed nvidia-smi query

--- kalao/rtc/gpu_control.py
+++ kalao/rtc/gpu_control.py
@@ -10,9 +10,36 @@ gpu_control.py is part of the KalAO Instrument Control Software
 (KalAO-ICS).
 """
 import subprocess
+import io
 import pprint
 
 def full_status():
+    '''
+    Reads the status of the nvidia GPU using the nvidia-smi command with csv output and returns it as a dictionary
+
+    :return: status_dict a dictionary contining all the gpu status values
+    '''
+
+    out_str = subprocess.run(['nvidia-smi', '--query-gpu=memory.total,memory.used,memory.free,temperature.gpu,power.draw,utilization.gpu,utilization.memory','--format=csv,nounits'], capture_output=True).stdout.decode('utf8')
+
+    status_frame = pd.read_csv(io.StringIO(out_str))
+    #status_frame.columns = status_frame.columns.str.strip()
+    status_frame.columns = [c.split(' ')[0] for c in list(status_frame.columns.str.strip())]
+
+    status_frame = status_frame.to_dict('records')[0]
+
+    status_dict = {}
+
+    status_dict['gpu_current_temp'] = status_frame['temperature.gpu']
+    status_dict['gpu_power_draw'] = status_frame['power.draw']
+    status_dict['gpu_used_memory'] = status_frame['memory.used']
+    status_dict['gpu_free_memory'] = status_frame['memory.free']
+    status_dict['gpu_load'] = status_frame['utilization.gpu']
+
+    return status_dict
+
+
+def full_status_parse():
     '''
     Reads the status of the nvidia GPU using the nvidia-smi command and returns it as a dictionary
 
@@ -48,3 +75,35 @@ def status():
 
 
     return status_dict
+
+
+'''
+"memory.total"
+Total installed GPU memory.
+
+"memory.used"
+Total memory allocated by active contexts.
+
+"memory.free"
+Total free memory.
+
+
+"temperature.gpu"
+ Core GPU temperature. in degrees C.
+
+"temperature.memory"
+ HBM memory temperature. in degrees C.
+ 
+"power.draw"
+The last measured power draw for the entire board, in watts. Only available if power management is supported. This reading is accurate to within +/- 5 watts.
+
+"utilization.gpu"
+Percent of time over the past sample period during which one or more kernels was executing on the GPU.
+The sample period may be between 1 second and 1/6 second depending on the product.
+
+"utilization.memory"
+Percent of time over the past sample period during which global (device) memory was being read or written.
+The sample period may be between 1 second and 1/6 second depending on the product.
+
+
+'''
