5dc5b490e0cf706cc4be3c7c2a4744c4d1b1c3ce
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-01-21 17:20:35 +0100

    Updating service scripts

--- scripts/kalao-database_updater.sh
+++ scripts/kalao-database_updater.sh
@@ -4,4 +4,4 @@ source /home/kalao/kalao-venv/bin/activate
 
 cd /home/kalao/kalao-ics/kalao/utils
 
-python3 /home/kalao/kalao-ics/kalao/utils/database_updater.py
+python /home/kalao/kalao-ics/kalao/utils/database_updater.py
