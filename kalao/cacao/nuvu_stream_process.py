0053f7b3d549514580cf00836867134d36861fd8
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-02-11 10:38:04 +0100

    Created nuvu stream processing script

---
+++ kalao/cacao/nuvu_stream_process.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Filename : nuvu_stream_process
+# @Date : 2022-02-11-10-21
+# @Project: KalAO-ICS
+# @AUTHOR : Janis Hagelberg
+
+"""
+nuvu_stream_process.py is part of the KalAO Instrument Control Software
+(KalAO-ICS). 
+"""
+
+from pyMilk.interfacing.isio_shmlib import SHM
+from time import sleep
+from signal import signal, SIGINT
+from sys import exit
+
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('\nSIGINT or CTRL-C detected. Exiting.')
+    # TODO close stream
+    #system.camera_service('start')
+    #print('Restarted kalao_camera service')
+    exit(0)
+
+
+def run():
+    # initialise stream
+    cam = SHM('nuvu_raw')
+
+    while True:
+        data = cam.get_data(check=True)[:, ::8]
+
+        shm_wr = SHM('nuvu_stream', data, # 30x30 int16 np.array
+                 location=-1, # CPU
+                 shared=True, # Shared
+                )
+
+
+if __name__ == '__main__':
+    # Tell Python to run the handler() function when SIGINT is recieved
+    signal(SIGINT, handler)
+
+    run()
