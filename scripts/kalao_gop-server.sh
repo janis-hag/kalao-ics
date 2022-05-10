3606386ea9eac87a3c6c68b11b589575ec846c20
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-05-10 14:30:14 +0200

    Adding GOP server as systemd service

--- scripts/kalao_gop-server.sh
+++ scripts/kalao_gop-server.sh
@@ -3,6 +3,6 @@
 source /home/kalao/kalao-venv/bin/activate
 source /home/kalao/kalao-config/_bash_aliases
 
-cd /home/kalao/kalao-ics/kalao/tcs_communication
+cd /home/kalao/kalao-ics/tcs_communication
 
-python /home/kalao/kalao-ics/kalao/tcs_communication/KalAO_GOP_server.py
+python /home/kalao/kalao-ics/tcs_communication/KalAO_GOP_server.py
