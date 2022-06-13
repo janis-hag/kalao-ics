52191006e5ed39949e18bc0729f1ae710abcb938
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-06-13 05:43:07 +0200

    Offsets

--- tcs_communication/t120.py
+++ tcs_communication/t120.py
@@ -15,18 +15,37 @@
 #import time
 #sys.path.append("/home/weber/src/pymod_libipc/")
 #sys.path.append("/home/weber/src/pymod_libgop/")
+
+import os
+
+from pathlib import Path
+from configparser import ConfigParser
+
 from tcs_communication.pyipc import pymod_libipc as ipc
 #import tcs_communication.pygop as gop
 
 from kalao.utils import database, kalao_time
 
-timeout   = 2
+# Read config file
+parser = ConfigParser()
+config_path = os.path.join(Path(os.path.abspath(__file__)).parents[1], 'kalao.config')
+parser.read(config_path)
 
-host      = "glslogin2.ls.eso.org"
-symb_name = "inter"
-rcmd      = "ipcsrv"
-port      = 12345
-semkey    = 1000
+#timeout   = 2
+
+#host      = "glslogin1.ls.eso.org"
+#symb_name = "inter"
+#rcmd      = "ipcsrv"
+#port      = 12345
+#semkey    = 1000
+
+host = parser.get('T120', 'Host')
+symb_name = parser.get('T120', 'symb_name')
+rcmd = parser.get('T120', 'rcmd')
+port = parser.getint('T120', 'Port')  # only for inet connection
+semkey = parser.getint('T120', 'semkey')  # only for inet connection
+timeout = parser.getint('T120', 'timeout')  # only for inet connection
+
 
 #
 # The connection to ipcsrv on <host>:
@@ -55,3 +74,46 @@ def send_offset(delta_alt, delta_az):
         offset_cmd = '@offset '+str(delta_alt) +' '+str(delta_az)
         ipc.send_cmd(offset_cmd, timeout, timeout)
 
+# def get_status():
+#     _t120_print_and_log(f'Sending {delta_alt} and {delta_az} offsets')
+#     socketId = ipc.init_remote_client(host, symb_name, rcmd, port, semkey)
+#     # print ("ipc.init_remote_client, returns:",socketId)
+#     if (socketId <= 0):
+#         _t120_print_and_log('Error connecting to T120')
+#         return -1
+#     else:
+#         #status_cmd = '@offset ' + str(delta_alt) + ' ' + str(delta_a)
+#         #ipc.send_cmd(offset_cmd, timeout, timeout)
+#
+#
+#         print ("wait");
+#         status = ipc.shm_wait(timeout)
+#         print ("ipc.shm_wait returns:", status)
+#         if (status<0):
+#           ipc.shm_free()
+#           sys.exit(-1)
+#
+#         print ("ini_shm_kw");
+#         ipc.ini_shm_kw()
+#
+#         print ("put_shm_kw COMMAND");
+#         ipc.put_shm_kw("COMMAND","@t120_get_positions")
+#
+#         print ("shmack");
+#         ipc.shm_ack()
+#         print ("shmwack");
+#         ipc.shm_wack()
+#         #
+#         # returns: ob.alpha ob.delta te.alpcons te.delcons scrmesuazi scrmesuele cupposmes
+#         #
+#         print ("get_shm_kw");
+#         returnList = ipc.get_shm_kw("scrmesuazi")
+#         azi = returnList[1]
+#         print ("get_shm_kw");
+#         returnList = ipc.get_shm_kw("scrmesuele")
+#         ele = returnList[1]
+#
+#         print("azi=",azi,"ele=",ele," <======================================")
+#
+#         print ("shmfree");
+#         ipc.shm_free()
