38640b716244f6062dd42809bc0dba0c28a29a66
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-10-18 15:01:00 +0200

    Updating __init__.py docstrings

--- kalao/plc/__init__.py
+++ kalao/plc/__init__.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Filename : __init__.pyt
+# @Date : 2022-10-18-14-56
+# @Project: KalAO-ICS
+# @AUTHOR : Janis Hagelberg
+
+"""
+__init__.py is part of the KalAO Instrument Control Software
+(KalAO-ICS).
+
+KalAO PLC control module.
+
+This package is used to control the calibration unit, the flip mirror, the shutter, the laser source,
+the tungsten source, the ADC motors, and the filterwheel.
+"""
+
 __all__ = ["core", "calib_unit", "flip_mirror", "shutter", "laser", "tungsten", "adc", "filterwheel"]
 from .core import *
 from .shutter import *
