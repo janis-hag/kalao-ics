cbae0ab15c83f64461cd5a8661e173e4ee51737c
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-11-02 15:45:37 +0100

    Cleaning telemetry.py code

--- kalao/cacao/telemetry.py
+++ kalao/cacao/telemetry.py
@@ -85,8 +85,6 @@ def _get_stream(name, min_value, max_value, sigma_clip=True):
 		if sigma_clip:
 			data, min_value, max_value = stats.sigmaclip(data,low=2.0, high=2.0)
 
-
-		#stream.close()
 		if len(data.shape) == 1:
 			# One dimensional stream
 			return {"data": data.flatten().tolist(), "width": 1, "height": data.shape[0], "min": min_value,
@@ -114,13 +112,13 @@ def get_stream_data(shm_stream, name, min_value, max_value):
 		try:
 			data = shm_stream.get_data(check=False)
 			list = data.flatten().tolist()
-					#stream.close()
+
 			if len(data.shape) == 1:
 				# One dimensional stream
 				width = 1
 				height = data.shape[0]
 			else:
-				width =  data.shape[1]
+				width = data.shape[1]
 				height = data.shape[0]
 			return {"data": list, "width": width, "height": height, "min": min(list), "max": max(list)}
 		except:
@@ -141,7 +139,7 @@ def streams(realData=True):
 		stream_list["dm01disp"] = _get_stream(name="dm01disp", min_value=-1.75, max_value=1.75)
 		stream_list["shwfs_slopes_flux"] = _get_stream(name="shwfs_slopes_flux", min_value=0, max_value=4*(2**16-1))
 		stream_list["aol1_mgainfact"] = _get_stream(name="aol1_mgainfact", min_value=0, max_value=1)
-		##streams["aol1_modeval"] = _get_stream("aol1_modeval", -1.75, 1.75) #TODO: uncomment when modal control is working
+		# streams["aol1_modeval"] = _get_stream("aol1_modeval", -1.75, 1.75) # TODO: uncomment when modal control is working
 
 		return stream_list
 
@@ -150,7 +148,7 @@ def telemetry_save(stream_list):
 	telemetry_data = {}
 
 	# Create the in-memory "file"
-	#temp_out = io.StringIO()
+	# temp_out = io.StringIO()
 
 	# # NUVU process
 	# #check if fps exists and is running
@@ -163,24 +161,24 @@ def telemetry_save(stream_list):
 	#
 	# 	# Check if it's running
 	# 	if fps_nuvu.RUNrunning==1:
-	# 		telemetry["nuvu_temp_ccd"]          = fps_nuvu["nuvu_accquire.temp_ccd"]
-	# 		telemetry["nuvu_temp_controller"]   = fps_nuvu["nuvu_accquire.temp_controller"]
-	# 		telemetry["nuvu_temp_power_supply"] = fps_nuvu["nuvu_accquire.temp_power_supply"]
-	# 		telemetry["nuvu_temp_fpga"]         = fps_nuvu["nuvu_accquire.temp_fpga"]
-	# 		telemetry["nuvu_temp_heatsink"]     = fps_nuvu["nuvu_accquire.temp_heatsink"]
-	# 		telemetry["nuvu_emgain"]            = fps_nuvu["nuvu_accquire.emgain"]
-	# 		telemetry["nuvu_exposuretime"]      = fps_nuvu["nuvu_accquire.exposuretime"]
+	# 		telemetry["nuvu_temp_ccd"]          = fps_nuvu["nuvu_acquire.temp_ccd"]
+	# 		telemetry["nuvu_temp_controller"]   = fps_nuvu["nuvu_acquire.temp_controller"]
+	# 		telemetry["nuvu_temp_power_supply"] = fps_nuvu["nuvu_acquire.temp_power_supply"]
+	# 		telemetry["nuvu_temp_fpga"]         = fps_nuvu["nuvu_acquire.temp_fpga"]
+	# 		telemetry["nuvu_temp_heatsink"]     = fps_nuvu["nuvu_acquire.temp_heatsink"]
+	# 		telemetry["nuvu_emgain"]            = fps_nuvu["nuvu_acquire.emgain"]
+	# 		telemetry["nuvu_exposuretime"]      = fps_nuvu["nuvu_acquire.exposuretime"]
 	#
 	# else:
 	# 	pass # Return empty streams
 
 	# NUVU process
-	#check if SHM exists and is running
+	# check if SHM exists and is running
 	nuvu_exists, nuvu_stream_path = check_stream("nuvu_raw")
 
 	server = libtmux.Server()
 	try:
-		session = server.find_where({ "session_name": "nuvu_ctrl" })
+		session = server.find_where({"session_name": "nuvu_ctrl"})
 	except:
 		# TODO specify more precise exception
 		session = False
@@ -197,56 +195,56 @@ def telemetry_save(stream_list):
 
 		# Check if it's running
 		#if fps_nuvu.RUNrunning==1:
-		telemetry_data["nuvu_temp_ccd"]          = stream_keywords['T_CCD']
-		telemetry_data["nuvu_temp_controller"]   = stream_keywords['T_CNTRLR']
+		telemetry_data["nuvu_temp_ccd"] = stream_keywords['T_CCD']
+		telemetry_data["nuvu_temp_controller"] = stream_keywords['T_CNTRLR']
 		telemetry_data["nuvu_temp_power_supply"] = stream_keywords['T_PSU']
-		telemetry_data["nuvu_temp_fpga"]         = stream_keywords['T_FPGA']
-		telemetry_data["nuvu_temp_heatsink"]     = stream_keywords['T_HSINK']
-		telemetry_data["nuvu_emgain"]            = stream_keywords['EMGAIN']
-		telemetry_data["nuvu_detgain"]            = stream_keywords['DETGAIN']
-		telemetry_data["nuvu_exposuretime"]      = stream_keywords['EXPTIME']
-		telemetry_data["nuvu_mframerate"] 		= stream_keywords['MFRATE']
+		telemetry_data["nuvu_temp_fpga"] = stream_keywords['T_FPGA']
+		telemetry_data["nuvu_temp_heatsink"] = stream_keywords['T_HSINK']
+		telemetry_data["nuvu_emgain"] = stream_keywords['EMGAIN']
+		telemetry_data["nuvu_detgain"] = stream_keywords['DETGAIN']
+		telemetry_data["nuvu_exposuretime"] = stream_keywords['EXPTIME']
+		telemetry_data["nuvu_mframerate"] = stream_keywords['MFRATE']
 
 	else:
-		pass # Return empty streams
-
+		# Return empty streams
+		pass
 
 	# SHWFS process
-	#check if fps exists and is running
+	# check if fps exists and is running
 	shwfs_exists, shwfs_fps_path = check_fps("shwfs_process")
 
-	pixel_scale = 5.7929690265142/5 # APO-Q-P240-R8,6 FOV per subap / pixels_per_subap
+	# APO-Q-P240-R8,6 FOV per subap / pixels_per_subap
+	pixel_scale = 5.7929690265142/5
 
 	if shwfs_exists:
 		if stream_list['fps_slopes'] is None:
 			stream_list['fps_slopes'] = fps("shwfs_process")
 
 		# Check if it's running
-		if stream_list['fps_slopes'].RUNrunning==1:
+		if stream_list['fps_slopes'].RUNrunning == 1:
 			telemetry_data["slopes_flux_subaperture"] = stream_list['fps_slopes'].get_param_value_float('flux_subaperture')
-			telemetry_data["slopes_residual_pix"]     = stream_list['fps_slopes'].get_param_value_float('residual')
-			telemetry_data["slopes_residual_arcsec"]  = stream_list['fps_slopes'].get_param_value_float('residual')*pixel_scale
+			telemetry_data["slopes_residual_pix"] = stream_list['fps_slopes'].get_param_value_float('residual')
+			telemetry_data["slopes_residual_arcsec"] = stream_list['fps_slopes'].get_param_value_float('residual')*pixel_scale
 
 	# Tip/tilt stream
-	#check if fps exists and is running
+	# check if fps exists and is running
 	tt_exists, tt_fps_path = check_stream("dm02disp")
 
 	if tt_exists:
 		if stream_list['tt_stream'] is None:
 			stream_list['tt_stream'] = SHM("dm02disp")
-			#stream_list['tt_stream'].close()
-		#else:
-			#stream_list['tt_stream'] = SHM("dm02disp")
+			# stream_list['tt_stream'].close()
+		# else:
+			# stream_list['tt_stream'] = SHM("dm02disp")
 
 		# Check turned off to prevent timeout. Data may be obsolete
 		tt_data = stream_list['tt_stream'].get_data(check=False)
-
 
 		telemetry_data["pi_tip"] = float(tt_data[0])
 		telemetry_data["pi_tilt"] = float(tt_data[1])
 
 	# looopRUN process
-	#check if fps exists and is running
+	# check if fps exists and is running
 	looprun_exists, looprun_fps_path = check_fps("loopRUN-1")
 
 	if looprun_exists:
@@ -254,11 +252,11 @@ def telemetry_save(stream_list):
 			stream_list['loopRUN'] = fps("loopRUN-1")
 
 		# Check if it's running
-		if stream_list['loopRUN'].RUNrunning==1:
+		if stream_list['loopRUN'].RUNrunning == 1:
 			telemetry_data["loop_gain"] = stream_list['loopRUN'].get_param_value_float('loopgain')
 			telemetry_data["loop_mult"] = stream_list['loopRUN'].get_param_value_float('loopmult')
 			# loopOn 0 = OFF, 1 = ON
-			telemetry_data["loop_on"]  = stream_list['loopRUN'].get_param_value_int('loopON')
+			telemetry_data["loop_on"] = stream_list['loopRUN'].get_param_value_int('loopON')
 			if telemetry_data["loop_on"] == 1:
 				telemetry_data["loop_on"] = 'ON'
 			elif telemetry_data["loop_on"] == 0:
@@ -266,9 +264,10 @@ def telemetry_save(stream_list):
 
 	database.store_telemetry(telemetry_data)
 
-	#return nuvu_stream, tt_stream, fps_slopes
+	# return nuvu_stream, tt_stream, fps_slopes
 
 	return 0
+
 
 def wfs_illumination_fraction(wfs_threshold):
 	"""
@@ -277,7 +276,7 @@ def wfs_illumination_fraction(wfs_threshold):
 	:return:  subaprtures summed flux
 	"""
 
-	# TODO implement masking prodedure in order to only consider usfeul subaps
+	# TODO implement masking procedure in order to only consider useful subaps
 
 	shwfs_stream = _get_stream("shwfs_slopes_flux", 0, 4*(2**16-1))
 
@@ -285,8 +284,6 @@ def wfs_illumination_fraction(wfs_threshold):
 
 	illuminated_pupil_fraction = (shwfs_array > wfs_threshold).sum() / len(shwfs_array)
 
-	#stream.close()
-
-	#TODO reject subaps out of centering zone
+	# TODO reject subaps out of centering zone
 
 	return illuminated_pupil_fraction
