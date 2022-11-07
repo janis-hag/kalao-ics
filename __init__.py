1092877eb200008e8baabb176626bbeeac725cdd
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-11-07 15:40:08 +0100

    Documenting code

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Filename : __init__.py
+# @Date : 2022-11-07-15-39
+# @Project: KalAO-ICS
+# @AUTHOR : Janis Hagelberg
+
+"""
+__init__.py is part of the KalAO Instrument Control Software
+(KalAO-ICS). 
+"""
+
+__all__ = ['kalao', 'sequencer', 'tcs_communication']
--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'kalao/filterwheel']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -62,3 +62,5 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
