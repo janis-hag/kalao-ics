f0683ccf0ada47295a381bf0b3a6a85af0b603ea
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-11-07 16:54:42 +0100

    Updating documenation

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.napoleon']
 
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
