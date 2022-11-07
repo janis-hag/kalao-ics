147da4a6ac7d8403b2e65752330587ed18639e2e
Janis Hagelberg <janis.hagelberg@unige.ch>
2022-11-07 16:43:22 +0100

    Documenting code

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'plc_stub', 'test_client']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'plc_stub', 'test_client', 'FLI']
 
 
 # -- Options for HTML output -------------------------------------------------
