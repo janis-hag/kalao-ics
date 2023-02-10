d349e039b5a59d2b24963d96241989ceb5ac7d88
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-02-10 10:33:41 +0100

    Adding set -x in enable_systems.sh

--- kalao-config/enable_systems.sh
+++ kalao-config/enable_systems.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+set -x
 
+# Enabling/installing services
 systemctl --user enable kalao_sequencer.service
 systemctl --user enable kalao_camera.service
 systemctl --user enable kalao_database-updater.service
@@ -8,6 +10,7 @@ systemctl --user enable kalao_gop_server.service
 systemctl --user enable kalao_safety-watchdog.service
 systemctl --user enable kalao_pump-watchdog.service
 
+# Starting services
 systemctl --user start kalao_sequencer.service
 systemctl --user start kalao_camera.service
 systemctl --user start kalao_database-updater.service
@@ -15,3 +18,9 @@ systemctl --user start kalao_flask-gui.service
 systemctl --user start kalao_gop_server.service
 systemctl --user start kalao_safety-watchdog.service
 systemctl --user start kalao_pump-watchdog.service
+
+# Printing out status
+systemctl --user status
+
+
+set +x
