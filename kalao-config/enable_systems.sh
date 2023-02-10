87cb9fbc8bac77732816d260d1acf2e97901d6d3
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-02-10 10:29:54 +0100

    Adding services start in enable_systems.sh

--- kalao-config/enable_systems.sh
+++ kalao-config/enable_systems.sh
@@ -2,8 +2,16 @@
 
 systemctl --user enable kalao_sequencer.service
 systemctl --user enable kalao_camera.service
-systemctl --user enable kalao_database_updater.service
-systemctl --user enable kalao_flask_gui.service
+systemctl --user enable kalao_database-updater.service
+systemctl --user enable kalao_flask-gui.service
 systemctl --user enable kalao_gop_server.service
 systemctl --user enable kalao_safety-watchdog.service
 systemctl --user enable kalao_pump-watchdog.service
+
+systemctl --user start kalao_sequencer.service
+systemctl --user start kalao_camera.service
+systemctl --user start kalao_database-updater.service
+systemctl --user start kalao_flask-gui.service
+systemctl --user start kalao_gop_server.service
+systemctl --user start kalao_safety-watchdog.service
+systemctl --user start kalao_pump-watchdog.service
