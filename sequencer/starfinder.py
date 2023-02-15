b48d9e2c51b676526ba0abe2a4845205aa2b9ed6
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-02-15 14:16:57 +0100

    Major changes to starfinder.py find_star function including tweaking the lumino factor by halving it and additional debugging

--- sequencer/starfinder.py
+++ sequencer/starfinder.py
@@ -198,7 +198,8 @@ def find_star(image_path, spot_size=7, estim_error=0.05, nb_step=5):
     lumino = np.float32((hist * bin_edges[:-1]).sum() / hist.sum() * 10) / 2
 
     #if lumino < image.max():
-    if lumino < image.max():
+    # TODO this quality check doesn't make much sense
+    if 3 * lumino < image.max():
         # Dirty hack in the black box...
         # Image quality insufficient for centering
         system.print_and_log(
