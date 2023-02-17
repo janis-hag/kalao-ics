a21dfdb01b22506201d01020d861d956ca91b178
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-02-17 11:52:50 +0100

    Add debugging print in wfs_centering

--- kalao/cacao/aocontrol.py
+++ kalao/cacao/aocontrol.py
@@ -130,7 +130,8 @@ def wfs_centering(tt_threshold):
         tip_offset = fps_bmc.get_param_value_float("ttm_tip_offset")
         tilt_offset = fps_bmc.get_param_value_float("ttm_tilt_offset")
 
-        print(f'tip_offset = {tip_offset}, tilt_offset = {tilt_offset}')
+        print(f'tip = {tip}, tilt = {tilt}, tip_offset = {tip_offset}, tilt_offset = {tilt_offset}'
+              )
 
         if tip_offset - tip < tt_threshold:
             tip_centered = True
