b5f071308ce244709e0620be4356175dec6e3242
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-10-28 09:39:31 +0200

    Decreasing WFS_illumination_threshold

--- kalao_config.py
+++ kalao_config.py
@@ -231,7 +231,7 @@ class T120:
 
 
 class AO:
-    WFS_illumination_threshold = 4000
+    WFS_illumination_threshold = 2000
     WFS_illumination_fraction = 0.4
     WFS_centering_precision = 0.1
     WFS_centering_timeout = 30
