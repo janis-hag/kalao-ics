58ff99b2259f4c8c67e280cf7dca3907ed587067
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-10-30 04:15:29 +0100

    Decreasing faint star dit value

--- sequencer/seq_command.py
+++ sequencer/seq_command.py
@@ -468,7 +468,7 @@ def target_observation(**seq_args):
     if centering == 'aut' or kao == 'AO':
         acq_dit = config.FLI.exp_time
         if float(mag_v) > 9:
-            acq_dit = 60
+            acq_dit = 20
 
         if starfinder.center_on_target(kao=kao, dit=acq_dit) == -1:
             system.print_and_log("Error: problem with center on target")
