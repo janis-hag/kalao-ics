82b28be5d2641f0f1b59f65de4f31c883de88453
Janis Hagelberg <janis.hagelberg@unige.ch>
2023-10-31 05:25:01 +0100

    Tuning check_wfs_flux

--- kalao_config.py
+++ kalao_config.py
@@ -237,7 +237,7 @@ class T120:
 
 class AO:
     WFS_illumination_threshold = 1000
-    WFS_illumination_fraction = 0.3
+    WFS_illumination_fraction = 0.35
     WFS_centering_precision = 0.1
     WFS_centering_timeout = 30
     WFS_centering_slope_threshold = 0.005
